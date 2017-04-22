64069172a132223a780f105c4a33007acab4326e
Kasper Holdum <kasper@holdum.net>
2017-04-23 01:33:37 +0200

    Map representation, player location and isOpenXXX functions

--- WarpShift.Console/Program.fs
+++ WarpShift.Console/Program.fs
@@ -1,7 +1,58 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿type OpenWalls =
+| Top
+| Bottom
+| Left
+| Right
+| TopLeft
+| TopRight
+| TopBottom
+| TopLeftRight
+| TopLeftBottom
+| TopLeftBottomRight
+| LeftRight
+| LeftBottom
+| LeftRightBottom
+| BottomRight
+
+type MapSpot =
+| Goal
+| Free of OpenWalls
+| Closed
+
+let map: MapSpot[,] = 
+    array2D [|
+        [|Closed; Free Top|];
+        [|Free Left; Goal|]|]
+
+let startPlayerLoc = [0;1]
+
+
+
+
+
+let isOpenTop (s : OpenWalls) =
+    match s with 
+    | _ -> true
+
+let isOpenBottom (s:OpenWalls) = 
+    match s with
+    | _ -> true
+
+let isOpenRight (s:OpenWalls) =
+    match s with
+    | _ -> true
+
+let isOpenLeft (s : OpenWalls) =
+    match s with
+    | Left | TopLeft | TopLeftRight 
+    | LeftRightBottom | LeftBottom | LeftRight 
+    | TopLeftBottomRight | TopLeftBottom | TopLeftRight -> true
+    | _ -> false
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
     0 // return an integer exit code
+
+
+
